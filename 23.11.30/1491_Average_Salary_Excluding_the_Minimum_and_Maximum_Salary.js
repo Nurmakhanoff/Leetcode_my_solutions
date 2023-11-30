e0cd313ff7e68df992083d2e70094754c2178394
Kuat <136558009+Nurmakhanoff@users.noreply.github.com>
2023-11-30 11:16:22 +0600

    Create 1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.js

---
+++ 23.11.30/1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.js
@@ -0,0 +1,47 @@
+/*
+You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+
+Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
+
+ 
+
+Example 1:
+
+Input: salary = [4000,3000,1000,2000]
+Output: 2500.00000
+Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
+Example 2:
+
+Input: salary = [1000,2000,3000]
+Output: 2000.00000
+Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
+Average salary excluding minimum and maximum salary is (2000) / 1 = 2000
+ 
+
+Constraints:
+
+3 <= salary.length <= 100
+1000 <= salary[i] <= 106
+All the integers of salary are unique.
+*/
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    let max=salary[0];
+    let min=salary[0];
+    let ans=0;
+    
+    for(let i=0; i<=salary.length-1; i++){
+        ans=ans+salary[i];
+        if(max<salary[i]){
+            max=salary[i];
+        }
+        else if(min>salary[i]){
+            min=salary[i];
+        }
+    }
+    return (ans-(min+max))/(salary.length-2);
+};
